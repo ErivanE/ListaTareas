df358370791e7f3401426bb4c003111868b5ce53
Ivan Elizalde <ielizalde185@gmail.com>
2022-03-21 15:46:47 -0600

    0.1 ahora puedo agregar mas tareas, max10

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main(){
 	char opcion;
 	char opcionSalir;
-	string tarea;
+	int contador=0;
+	int numeroTarea;
+	string tarea[10];
 	
 	do{
 		cout<<"Agregar tarea? s/n: "; 
@@ -13,20 +15,26 @@ int main(){
 		if(opcion == 's' || opcion == 'S'){
 			cin.ignore();
 			cout<<"ingresa la tarea por realizar: ";
-			getline(cin,tarea);
+			getline(cin,tarea[contador]);
 			cout<<endl;
+			contador++;
 		}
 	
 		cout<<"Eliminar tarea?: s/n: ";
 		cin>>opcion;
 		if(opcion == 's'|| opcion == 'S'){
-			cout<<"eliminando tarea..."<<endl;
-			tarea="";
+			//que numero de tarea vas a borrar?
+			cout<<"Tarea a eliminar? ";
+			cin>> numeroTarea; 		//Guarda el numero en la variable numeroTarea
+			tarea[numeroTarea]="";	//deja en blanco el strig[numeroTarea], es decir el string que se selecciono
 			cout<<"Tarea eliminada"<<endl;
 		}
 		
-		cout<<"\n...:::Tarea pendiente:::..."<<endl;
-		cout<<tarea<<endl;
+		cout<<"\n...:::Tareas pendientes:::..."<<endl;
+		//muestra TODAS las tareas creadas.
+		for(int i=0;i<=contador; i++){
+			cout<<tarea[i]<<endl;		//con esto hacemos un recorrido desde 0 hasta contador, osease la ultima tarea agregada
+		}
 		
 		cout<<"Salir? s/n"<<endl;
 		cin>>opcionSalir;
