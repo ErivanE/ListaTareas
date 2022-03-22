d001c2c6f97bb327713e931f17fb2484ed7d7de3
Ivan Elizalde <ielizalde185@gmail.com>
2022-03-21 21:03:39 -0600

    0.3 se agregan funciones

--- main.cpp
+++ main.cpp
@@ -1,104 +1,106 @@
+/*
+			0.3 
+	implementando funciones
+		Imprimir
+		pausa
+		limpiarPantalla
+	se movieron los case a funciones.
+		EliminarTarea
+		AgregarTarea
+		MostrarTareas
+*/
+#ifdef _WIN32
+	#include <windows.h>
+#endif
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
 using namespace std;
 
+int cantidadTareas = 0;
+int tareaSeleccionada;
+string tarea[10];
+
+void eliminarTarea();
+void imprimir(string texto);
+void limpiarPantalla();
+void mostrarTareas();
+void agregarTarea();
+void pausa();
+void reiniciarCin();
+
+
 int main(){
-	char opcion;
-	char opcionSalir;
-	int opcionMenu;
-	int contador=0;
-	int numeroTarea;
-	int tareaEliminar;
-	string tarea[10];
-	
+	int opcionMenu;	
 	do{
-		cout<<"Menu"<<endl;
-		cout<<"1. Agregar Tarea"<<endl;
-		cout<<"2. Eliminar Tarea"<<endl;
-		cout<<"3. Ver tareas"<<endl;
-		cout<<"4. Salir."<<endl;
+		limpiarPantalla();
+		imprimir("...:::MENU:::...");
+		imprimir("1. Agregar Tarea");
+		imprimir("2. Eliminar Tarea");
+		imprimir("3. Ver Tareas");
+		imprimir("4. Salir");
+		
 		cin>>opcionMenu;
-		cin.ignore();
+		reiniciarCin();
+		limpiarPantalla();
 		
 		switch(opcionMenu){
 			case 1:
-				cout<<"Ingresa la tarea a realizar: ";
-				getline(cin, tarea[contador]);
-				contador++;
-				system("cls");
+				agregarTarea();
 				break;
 			case 2:
-				cout<<"tarea a eliminar?: ";
-				cin>>numeroTarea;
-				cin.ignore();
-				tareaEliminar = numeroTarea-1;
-				tarea[tareaEliminar] = "";
-				cout<<"Tarea Eliminada"<<endl;
-				system("cls");
+				eliminarTarea();
 				break;
 			case 3:
-				cout<<"...:::Tareas Pendientes:::..."<<endl;
-				for(int i=0;i<=contador; i++){
-					cout<<"Tarea "<<i+1<<" : "<<tarea[i]<<endl; //con esto hacemos un recorrido desde 0 hasta contador, osease la ultima tarea agregada
-			 	}
-			 	cin.get();
-				system("cls");
+				imprimir("...:::Tareas Pendientes:::...");
+				mostrarTareas();
+				pausa();
 				break;
 			case 4:
-				cout<<"Saliendo...";
+				imprimir("Saliendo...");
 				break;
 			
-			default: cout<<"Ingresa una opcion correcta."<<endl;	
-		}
-				
-		
-		/*
-		cout<<"Agregar tarea? s/n: "; 
-		cin>>opcion;
-		cin.ignore();
-		if(opcion == 's' || opcion == 'S'){
-			cout<<"ingresa la tarea por realizar: ";
-			getline(cin,tarea[contador]);
-			cout<<endl;
-		}
-		
-		cout<<"Eliminar tarea?: s/n: ";
-		cin>>opcion;
-		cin.ignore();
-		if(opcion == 's'|| opcion == 'S'){
-			//que numero de tarea vas a borrar?
-			cout<<"Tarea a eliminar? ";
-			cin>> numeroTarea; 		//Guarda el numero en la variable numeroTarea
-			cin.ignore();
-			int tareaEliminar = numeroTarea-1;
-			tarea[tareaEliminar]="";	//deja en blanco el strig[numeroTarea], es decir el string que se selecciono
-			cout<<"Tarea eliminada"<<endl;
-		}
-		
-		
-		cout<<"\n...:::Tareas pendientes:::..."<<endl;
-		//muestra TODAS las tareas creadas.
-		for(int i=0;i<=contador; i++){
-			cout<<"Tarea "<<i+1<<" : "<<tarea[i]<<endl;		//con esto hacemos un recorrido desde 0 hasta contador, osease la ultima tarea agregada
-		}
-		
-		contador++;
-		
-		cout<<"\n\nSalir? s/n"<<endl;
-		cin>>opcionSalir;
-		cin.ignore();
-		*/
-		
+			default: 
+				imprimir("Selecciona una opcion valida");
+				pausa();
+		}	
 	}while(opcionMenu != 4);
 	
-	/*
-	string tarea = "";
-	cout<<"ingresa la tarea por realizar: "<<endl;
-	getline(cin,tarea);
-	cout<<"T1: "<<tarea<<endl;
-	int tamanio = tarea.length();
-	cout<<"tamaño: "<<tamanio<<endl;
-	*/
 	return 0;
 }
+
+void limpiarPantalla(){
+	#ifdef _WIN32
+		system("cls");
+	#else
+		system("clear");
+	#endif
+}
+void reiniciarCin(){
+	cin.ignore();
+}
+void imprimir(string texto){
+	cout<<texto<<endl;
+}
+void pausa(){
+	cout<<"\nPresiona para continuar."<<endl;
+	cin.get();
+}
+void mostrarTareas(){
+	for(int i=0;i<=(cantidadTareas - 1); i++){
+		cout<<"\nTarea "<<i+1<<" : "<<tarea[i]<<endl; //con esto hacemos un recorrido desde 0 hasta contador, osease la ultima tarea agregada
+	}
+}
+void agregarTarea(){
+	imprimir("Ingresa la tarea a realizar: ");
+	getline(cin, tarea[cantidadTareas]);
+	cantidadTareas++;
+}
+void eliminarTarea(){
+	imprimir("Tarea a eliminar?: ");
+	cin>>tareaSeleccionada;
+	reiniciarCin();
+	tarea[tareaSeleccionada-1] = "";
+	imprimir("Tarea Eliminada.");
+}
+
